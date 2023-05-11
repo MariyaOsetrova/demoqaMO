93ee7c12d5ff26ee1b2d9572c5f50633b9092dd7
mosetrova <mosetrova@solit-clouds.ru>
2023-05-11 14:11:11 +0300

    видео 2

--- src/test/java/ru/company/tests/TextBoxTests.java
+++ src/test/java/ru/company/tests/TextBoxTests.java
@@ -3,13 +3,14 @@ package ru.company.tests;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
 
 public class TextBoxTests {
     @Test
     void fillFormTest(){ // заполнить форму теста
         open ("https://demoqa.com/text-box"); //открываем сайт
-       // sleep(20000);
+     //   sleep(30000);
         $("#userName").setValue("ВвелаИмя"); // ввели в поле имя
         $("#userEmail").setValue("mail@mail.ru"); // ввели почту
         $("#currentAddress").setValue("Адрес1");// ввели адрес1
