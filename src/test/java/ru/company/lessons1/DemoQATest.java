48a1d4787b918c5f585aff5460e2e472477115d1
mosetrova <mosetrova@solit-clouds.ru>
2023-05-11 13:42:16 +0300

    видео 2

---
+++ src/test/java/ru/company/lessons1/DemoQATest.java
@@ -0,0 +1,37 @@
+package ru.company.lessons1;
+
+import org.junit.jupiter.api.*;
+
+public class DemoQATest {
+
+    @BeforeAll // обернет разом все тесты
+    static void beforAll(){
+        System.out.println("Первый самы и обернем ВСЕ тесты\n");
+    }
+
+    @AfterAll
+    static void afterAll(){
+        System.out.println("Самый последний после всех ТЕСТОВ\n");
+    }
+
+    @BeforeEach // обернет каждый тест
+    void beforEach(){
+        System.out.println("Выводим ПЕРЕД текстом\n");
+    }
+
+    @AfterEach // обернет каждый тест, стр 20-23 и потом еще 25-28
+    void AfterEach(){
+        System.out.println("Выводим ПОСЛЕ теста\n");
+
+    }
+
+    @Test // аннотация 1
+    void ferstTest(){
+        System.out.println("ПЕрвый тест, без MAIN, т.к поиск методов @Test по имени вызывает их\n");
+    }
+
+    @Test // аннотация 2
+    void secondTest(){
+        System.out.println("Второй тест, без MAIN, т.к поиск методов @Test по имени вызывает их\n");
+    }
+}
---
+++ src/test/java/ru/company/tests/TextBoxTests.java
@@ -0,0 +1,20 @@
+package ru.company.tests;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.*;
+
+public class TextBoxTests {
+    @Test
+    void fillFormTest(){ // заполнить форму теста
+        open ("https://demoqa.com/text-box"); //открываем сайт
+       // sleep(20000);
+        $("#userName").setValue("ВвелаИмя"); // ввели в поле имя
+        $("#userEmail").setValue("mail@mail.ru"); // ввели почту
+        $("#currentAddress").setValue("Адрес1");// ввели адрес1
+        $("#permanentAddress").setValue("Адрес2");// ввели адрес2
+        $("#submit").click();
+
+    }
+}
